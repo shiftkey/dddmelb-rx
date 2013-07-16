410e148eda7e5ff8bbee8d6e8a485eec9400f152
Brendan Forster <me@brendanforster.com>
2013-07-16 12:36:22 +0930

    added demos for querying and processing requests

---
+++ async/MakingWebRequests/MakingWebRequests/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net.Http;
+using System.Reactive.Linq;
+using System.Reactive.Threading.Tasks;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace MakingWebRequests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = "http://httpstat.us/404";
+            MakeAnObservableRequest(url).Wait();
+            //MakeAnAsyncRequest(url).Wait();
+        }
+
+        static IObservable<object> MakeAnObservableRequest(string url)
+        {
+            var client = new HttpClient();
+            var text = client.GetAsync(url)
+                             .ToObservable()
+                             .Select(response => response.Content.ReadAsStringAsync().Result)
+                             .Select(content => JsonConvert.DeserializeObject(content))
+                             .Catch<object, Exception>(ex =>
+                             {
+                                 Console.WriteLine("An exception occurred: {0}", ex);
+                                 return Observable.Return(new Object());
+                             });
+            return text;
+        }
+
+        static async Task MakeAnAsyncRequest(string url)
+        {
+            var client = new HttpClient();
+            var response = await client.GetAsync(url);
+            var text = await response.Content.ReadAsStringAsync();
+            var json = await JsonConvert.DeserializeObjectAsync(text); // this should explode
+        }
+    }
+}
---
+++ query/QueryDemo/QueryDemo/QueryModule.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Reactive.Linq;
+using System.Reactive.Threading.Tasks;
+using System.Web;
+
+namespace QueryDemo
+{
+    public class QueryModule : Nancy.NancyModule
+    {
+        public QueryModule()
+        {
+            Get["/search/{term}"] = parameters =>
+            {
+                // i should use the search terms here
+                // and probably use a proper search API
+                // if they even exist still
+                // but that's not the point
+                var first = HttpWebRequest.CreateHttp("http://www.google.com.au");
+                var second = HttpWebRequest.CreateHttp("http://www.bing.com");
+                var third = HttpWebRequest.CreateHttp("http://duckduckgo.com");
+
+                // start them all up
+                var allRequests = Observable.Merge(
+                    first.GetResponseAsync().ToObservable(),
+                    second.GetResponseAsync().ToObservable(),
+                    third.GetResponseAsync().ToObservable());
+
+                // give me the one that finishes first
+                var fastest = allRequests.FirstAsync();
+
+                // block until it gets here
+                var result = fastest.Wait();
+
+                // read the content and return it to the client
+                var reader = new StreamReader(result.GetResponseStream());
+                var content = Observable.FromAsync(reader.ReadToEndAsync);
+                var text = content.Wait();
+                return text;
+            };
+        }
+    }
+}
